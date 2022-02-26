f0079869ec38a6799d5d2b2f0dc1ec5a0fd93992
Alok Shukla <alokshukla994@gmail.com>
2022-02-26 14:32:25 +0530

    First Commit

--- model.py
+++ model.py
@@ -2,28 +2,70 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from utils import INPUT_SHAPE, batch_generator
 import argparse
 import os
+import ntpath
+import matplotlib.image as mpimg
+import random
+import matplotlib.pyplot as plt
+
 
 np.random.seed(0)
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    # tail = 'IMG/' + tail
+    return tail
+
+
+# print(data.iloc[1])
+def load_img_steering(datadir, data):
+  image_path = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+    image_path.append(os.path.join(datadir, center.strip()))
+    steering.append(float(indexed_data[3]))
+    # left image append
+    image_path.append(os.path.join(datadir,left.strip()))
+    steering.append(float(indexed_data[3])+0.15)
+    # right image append
+    image_path.append(os.path.join(datadir,right.strip()))
+    steering.append(float(indexed_data[3])-0.15)
+  image_paths = np.asarray(image_path)
+  steerings = np.asarray(steering)
+  return image_paths, steerings
+
+
+
+
+
 
 
 def load_data(args):
     """
     Load training data and split it into training and validation set
     """
-    data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'))
+    
+    columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+    data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'),  names = columns)
+
+    # data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'))
 
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
 
+    print(data_df.head())
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
 
     return X_train, X_valid, y_train, y_valid
+
+    
 
 
 def build_model(args):
@@ -32,11 +74,11 @@ def build_model(args):
     """
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-    model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Conv2D(36, (5, 5), activation='elu', strides=(2, 2)))
+    model.add(Conv2D(24, (5, 5), activation='elu', strides=(2, 2)))
+    model.add(Conv2D(48, (5, 5), activation='elu', strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='elu'))
+    model.add(Conv2D(64, (3, 3), activation='elu'))
     model.add(Dropout(args.keep_prob))
     model.add(Flatten())
     model.add(Dense(100, activation='elu'))
@@ -44,6 +86,7 @@ def build_model(args):
     model.add(Dense(10, activation='elu'))
     model.add(Dense(1))
     model.summary()
+    
 
     return model
 
@@ -63,9 +106,9 @@ def train_model(model, args, X_train, X_valid, y_train, y_valid):
     model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
                         args.samples_per_epoch,
                         args.nb_epoch,
-                        max_q_size=1,
+                        max_queue_size =1,
                         validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
-                        nb_val_samples=len(X_valid),
+                        validation_steps=len(X_valid),
                         callbacks=[checkpoint],
                         verbose=1)
 
@@ -83,7 +126,7 @@ def main():
     Load train/validation data set and train the model
     """
     parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
-    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default=os.getcwd())
     parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
     parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
     parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
